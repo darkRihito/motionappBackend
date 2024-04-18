f79775877d8e56d2c62b95ddb1ed6ceda0b8cea6
darkRihito <fieshidiq@gmail.com>
2024-04-19 00:08:06 +0700

    sprint1progress

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ import leaderboardRouter from "./src/router/leaderboard.router.js";
 import historychallengeRouter from "./src/router/historychallenge.router.js";
 import questionRouter from "./src/router/question.router.js";
 import practiceRouter from "./src/router/practice.router.js";
+import shopRouter from "./src/router/shop.router.js";
 import { isAuthenticated, isAdmin } from "./src/middleware/auth.js";
 
 // setting up the server
@@ -57,6 +58,7 @@ app.use("/leaderboard", leaderboardRouter);
 app.use("/challenge", historychallengeRouter);
 app.use("/question", questionRouter);
 app.use("/practice", practiceRouter);
+app.use("/shop", shopRouter);
 dotenv.config();
 
 if (process.env.NODE_ENV === "production") {
---
+++ src/controller/shop.controller.js
@@ -0,0 +1,29 @@
+import userModel from "../model/user.model.js";
+import ResponseHandler from "../utils/responseHandler.js";
+
+// Tukar poin
+export const buyAvatar = async (
+  /** @type import('express').Request */ req,
+  /** @type import('express').Response */ res,
+  next
+) => {
+  const userId = req.user.id;
+  const itemTarget = req.body.item_target;
+  const itemUrl = req.body.item_url;
+
+  try {
+    const data = await userModel.findById(userId);
+    if (data.challenge_point >= itemTarget) {
+      const response = await userModel.findByIdAndUpdate(userId, {
+        pict_url: itemUrl,
+      });
+      return next(
+        ResponseHandler.successResponse(res, 200, "successful", response)
+      );
+    } else {
+      return next(ResponseHandler.errorResponse(res, 500, "Poin Kurang"));
+    }
+  } catch (error) {
+    return next(ResponseHandler.errorResponse(res, 500, error.message));
+  }
+};
